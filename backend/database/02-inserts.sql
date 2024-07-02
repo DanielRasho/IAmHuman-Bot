a71a1d82079f739fbb356c8dde294030e36cd5ff
DanielRasho <ray22933@uvg.edu.gt>
2024-07-01 22:28:37 -0600

    feat: create database script initializers.

---
+++ backend/database/02-inserts.sql
@@ -0,0 +1,9 @@
+-- Dummy Data
+INSERT INTO member (username, password)
+values
+	('dummy', 123);
+
+-- INSERT INTO post ( author, title, tags, thumbnailpath, contentpath )
+-- values
+--	('dummy', 'title 1', 'art,Noveau', '/image/path', '/content/path'),
+--	('dummy', 'title 2', 'drawing,draft', '/image/path', '/content/path');
