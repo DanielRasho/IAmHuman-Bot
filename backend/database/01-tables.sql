696d68754de6903153205199dcb393d38019e534
DanielRasho <ray22933@uvg.edu.gt>
2024-07-02 14:27:51 -0600

    fix: rename table name from post to invitation

--- backend/database/01-tables.sql
+++ backend/database/01-tables.sql
@@ -4,12 +4,12 @@ create table member (
 	password VARCHAR(30) not null
 ); 
 
-create table post (
+create table invitation (
 	id serial primary key,
 	class VARCHAR(30) not null,
 	section INT not null,
 	missing_uses INT not null,
 	created_at timestamp default current_timestamp not null,
-	server_id text not null
+	server_id text not null,
 	role_id text not null
 );
