551cf30957e5455b9a5573b45167efe5ecad7b50
DanielRasho <ray22933@uvg.edu.gt>
2024-07-06 18:18:26 -0600

    feat: Create TextInput component

---
+++ web/src/components/TextInput.vue
@@ -0,0 +1,49 @@
+<template>
+  <input class="input" :type="type" @input="handleInput" :placeholder="placeholder" />
+</template>
+<script setup>
+const emit = defineEmits(['input'])
+
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'text'
+  },
+  placeholder: {
+    type: String,
+    default: ''
+  }
+})
+
+function handleInput(event) {
+  emit('input', event.target.value)
+}
+</script>
+<style scoped>
+.input {
+  border-color: #fdfcfc1a;
+  background-color: #1d191673;
+  color: white;
+
+  border-radius: 0.75rem;
+  border-width: 1px;
+  border-style: solid;
+
+  padding: 0.375rem 0.725rem;
+  margin-top: 0.5ch;
+  appearance: none;
+  line-height: 1.5rem;
+
+  font-size: 1rem;
+
+  outline: 2px solid transparent;
+
+  outline-offset: 2px;
+}
+.input:hover {
+  border-color: var(--grey-03);
+}
+.input:focus {
+  border-color: var(--primary);
+}
+</style>
