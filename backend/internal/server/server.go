1badc7bd2fe2f489af86857943676513fb1f98a8
DanielRasho <ray22933@uvg.edu.gt>
2024-07-07 13:04:00 -0600

    fix : update server CORS header options

--- backend/internal/server/server.go
+++ backend/internal/server/server.go
@@ -19,7 +19,7 @@ func (s *Server) Run() error {
 func NewServer() (*Server, error) {
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	db, err := database.New()
-	
+
 	router := gin.Default()
 	router.Use(CORSMiddleware())
 
@@ -73,17 +73,17 @@ func AuthRequiredMiddleware() gin.HandlerFunc {
 	}
 }
 func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(200)
-            return
-        }
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(200)
+			return
+		}
 
-        c.Next()
-    }
+		c.Next()
+	}
 }
