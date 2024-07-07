0cc2906f891f45fe48841087c59a7357d2931444
DanielRasho <ray22933@uvg.edu.gt>
2024-07-07 14:43:53 -0600

    feat: assignRole can retrieve data from backend and redirect to discord Ouath

--- web/src/views/AssignRoleView.vue
+++ web/src/views/AssignRoleView.vue
@@ -5,17 +5,52 @@
       <h1>Hola!</h1>
       <p>
         Este link te dara acceso a la clase
-        <span>POO</span>
-        seccion <span> 10 </span> del server.
+        <span>{{ class_subject }}</span>
+        seccion <span> {{ class_section }} </span> del server.
       </p>
-      <p>Usos restantes: <span> 15</span></p>
-      <ButtonPrimary class="login-btn" text="Authorize" pre-icon="fa-brands fa-discord" />
+      <ButtonPrimary
+        class="login-btn"
+        text="Authorize"
+        :is-disabled="isButtonDisable"
+        pre-icon="fa-brands fa-discord"
+        @clicked="navigateToAuth"
+      />
     </div>
   </div>
 </template>
 <script setup>
 import ButtonPrimary from '@/components/ButtonPrimary.vue'
-import ToolTip from '@/components/ToolTip.vue'
+import { APISettings } from '@/utils/apiConfig'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const ouath_uri = ref('')
+const class_subject = ref('...')
+const class_section = ref('...')
+const isButtonDisable = ref(true)
+
+function navigateToAuth() {
+  location.assign(ouath_uri.value)
+}
+
+onMounted(async () => {
+  const url = APISettings.baseURL + 'assign-role/?invitation_id=' + route.params.id
+  try {
+    const response = await fetch(url, {
+      method: 'GET'
+    })
+    const data = await response.json()
+    if (!response.ok) throw Error(data.message)
+    class_subject.value = data.invitation_data.class
+    class_section.value = data.invitation_data.section
+    ouath_uri.value = data.Oauth
+    isButtonDisable.value = false
+  } catch (err) {
+    alert('Cannot fetch invitation data\n' + err)
+  }
+})
 </script>
 <style scoped>
 .body {
