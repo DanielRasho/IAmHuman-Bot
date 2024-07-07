f56433935696a14696a570621053f17d0eaa77a8
DanielRasho <ray22933@uvg.edu.gt>
2024-07-06 18:19:05 -0600

    feat: Move dashboard view to its own folder

---
+++ web/src/views/Dashboard/CreateInvitation.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="section section--create">
+    <h1 class="header">Crear invitación</h1>
+    <div class="create-input">
+      <span class="input-tag">Clase</span>
+      <TextInput type="text" />
+    </div>
+    <div class="create-input">
+      <span class="input-tag">Section</span>
+      <TextInput type="text" />
+    </div>
+    <div class="create-input">
+      <span class="input-tag">Usos maximos</span>
+      <TextInput type="text" />
+    </div>
+    <div class="create-input">
+      <span class="input-tag">Sever ID</span>
+      <TextInput type="text" />
+    </div>
+    <div class="create-input">
+      <span class="input-tag">Role ID</span>
+      <TextInput type="text" />
+    </div>
+    <ButtonPrimary text="Create" is-disabled="false" />
+  </section>
+</template>
+<script setup>
+import ButtonPrimary from '@/components/ButtonPrimary.vue'
+import TextInput from '@/components/TextInput.vue'
+</script>
+<style scoped>
+.header {
+  color: var(--white);
+  font-weight: bold;
+  font-size: 2.2rem;
+  margin-bottom: 1rem;
+}
+.section {
+  margin-bottom: 3rem;
+}
+.create-input {
+  margin-bottom: 0.5rem;
+}
+.input-tag {
+  display: inline-block;
+  width: 14ch;
+}
+</style>
---
+++ web/src/views/Dashboard/DashboardView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="body">
+    <CreateInvitation />
+    <section class="section section--view">
+      <h1 class="header">Invitaciones Existentes</h1>
+    </section>
+  </div>
+</template>
+<script setup>
+import CreateInvitation from '@/views/Dashboard/CreateInvitation.vue'
+</script>
+<style scoped>
+.body {
+  width: 100%;
+  padding: 2rem;
+}
+</style>
