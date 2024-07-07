dd9e6b11d4f0029d94b5f9da0fdee339b87dd185
DanielRasho <ray22933@uvg.edu.gt>
2024-07-06 22:54:36 -0600

    feat: Style NotFound view

--- web/src/views/NotFound.vue
+++ web/src/views/NotFound.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Not found</div>
+  <div class="container">
+    <h1 class="header">404</h1>
+    <p class="message">Page not found</p>
+  </div>
 </template>
 <script setup></script>
-<style></style>
+<style scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.header {
+  font-size: 7rem;
+  font-weight: bold;
+  background: linear-gradient(to right, var(--green-light), var(--blue));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+</style>
